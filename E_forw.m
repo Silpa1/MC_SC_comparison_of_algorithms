ed8462aaef3d3a046afa397ab1877cec47b2ad0e
Silpa1 <102550086+Silpa1@users.noreply.github.com>
2022-08-26 12:28:33 -0500

    Delete E_forw.m

--- E_forw.m
+++
@@ -1,9 +0,0 @@
-function S = E_forw(x)
-global nx ny nt nc samp b1c
-s = zeros(nx,ny,nt,nc); 
-s = bsxfun(@times,x,reshape(b1c,[nx,ny,1,nc]));
-S=fft2c_mri(s);
-if ~isempty(samp) %cl: samp mask only when cartesian samp
-    S = bsxfun(@times,S,samp);
-end
-end
